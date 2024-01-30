410b360c46fda2cbbe73458d222ff178356fdadd
rangkumantik <155991805+rangkumantik@users.noreply.github.com>
2024-01-30 17:36:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function closeNav() {
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
